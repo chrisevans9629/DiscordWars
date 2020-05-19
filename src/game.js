9b3d4cb176a9b0d861deb64013ab5ff6004ea2fc
Chris Evans <chrisevans9629@gmail.com>
2020-05-18 21:06:14 -0500

    maximized game

--- src/game.js
+++ src/game.js
@@ -2,8 +2,8 @@ import Phaser from 'phaser';
 import Level1 from './Levels/level1';
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 800,
+    width: '100%',
+    height: '100%',
     scene: [Level1],
     parent: 'game-parent'
 };
