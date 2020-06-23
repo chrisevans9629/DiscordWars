8a074eeb62e912a4f9fdf3118563f9aa77cb31bb
Chris Evans <chrisevans9629@gmail.com>
2020-06-23 17:35:10 -0500

    fixed negative health

--- src/healthbar.ts
+++ src/healthbar.ts
@@ -4,8 +4,6 @@ import { ProgressBar } from "./ProgressBar";
 import { soundSystem } from "./game";
 
 export class HealthBar implements IHealthBar {
-   // private x:number;
-   // private y:number;
     private _health:number;
     get health(){
         return this._health;
@@ -22,9 +20,6 @@ export class HealthBar implements IHealthBar {
         this._maxHealth = hp;
         this.draw();
     }
-
-   // private valueRatio:number;
-   // bar: Phaser.GameObjects.Graphics;
     get isFullHealth(){
         return this.health >= this.maxHealth;
     }
@@ -34,20 +29,9 @@ export class HealthBar implements IHealthBar {
         this.progressBar = new ProgressBar(scene, x, y);
         this.progressBar.alpha = 0.5;
         this.progressBar.draw();
-        //this.bar = new Phaser.GameObjects.Graphics(scene);
-
-        //this.x = x;
-        //this.y = y;
-        //this.health = 100;
-        //this.maxHealth = 100;
-        //this.valueRatio = 76 / 100;
-
-        //this.draw();
-
     }
     addHealth(amt: number){
         let t = addHealth(amt, this);
-
         this.draw();
         let ratio = this.health/this.maxHealth;
         if(ratio >= 1){
@@ -56,29 +40,17 @@ export class HealthBar implements IHealthBar {
         else{
             soundSystem.playHealing(ratio);
         }
-
         return t;
     }
     setHealth(amt: number){
         this.health = amt;
         this.draw();
     }
-    decrease (amount: number)
-    {
-        this.health -= amount;
-
-        if (this.health < 0)
-        {
-            this.health = 0;
-        }
-
-        this.draw();
-
-        return (this.health === 0);
-    }
-
     draw ()
     {
+        if(this._health <= 0){
+            this._health = 0;
+        }
         this.progressBar.maxValue = this._maxHealth;
         this.progressBar.value = this._health;
         this.progressBar.draw();
